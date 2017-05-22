236fab1867452e2ef44b689032f7f23681955deb
Alisher778 <alisher.musurmonov89@gmail.com>
2017-05-22 16:29:02 -0400

    added on scroll change background color white and font color black

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$(document).scroll(function(){
+		var scrollTop = $(document).scrollTop();
+		console.log(scrollTop)
+	    if(scrollTop > 100){
+	  	
+	    	$('nav').css({'background-color':'#fff', 'color': '#000'});
+	
+	    }else{
+	    	$('nav').css({'background-color':'#fff', 'color': '#fff'})
+	    }
+  	});
+})
